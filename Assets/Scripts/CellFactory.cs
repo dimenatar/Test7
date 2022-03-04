4c70c4b45f2ffbb40fe01fd6b9e780a6131d694a
dimenatar <dimenatar25@gmail.com>
2022-03-04 23:30:55 +0300

    Grid spawn rocks

--- Assets/Scripts/CellFactory.cs
+++ Assets/Scripts/CellFactory.cs
@@ -8,18 +8,19 @@ public class CellFactory : MonoBehaviour
     [SerializeField] private CellBundle _cellBundle;
     [SerializeField] private CellContentBundle _cellContentBundle;
     [SerializeField] private GameObject _firstCube;
+
     public void SetUpCells(int rowCount, int columnCount, float cellXOffset, float cellYOffset, float rotation, float newrowXOffset, Vector3 firstCubePosition)
     {
         if (_cellBundle.Cells.Count < rowCount * columnCount) throw new System.Exception("not enough cells");
         Cell cell;
-        CellContent cellContent;
+        GameObject cellBox;
         for (int i = 0; i < rowCount; i++)
         {
             for (int j = 0; j < columnCount; j++)
             {
-                cell = _cellBundle.Cells[i * rowCount + columnCount];
-                //cell.SetUpCell(new Vector3(i * cellXOffset + firstCubePosition.x, 0, j * cellYOffset + firstCubePosition.z), _cellContentBundle.CellContents.Where(content => content.CellType == cell.CellType).Select(pref => pref.Prefab).FirstOrDefault());
-                Instantiate(_cellContentBundle.CellContents.Where(content => content.CellType == cell.CellType).Select(pref => pref.ContentPrefab).FirstOrDefault(), new Vector3(j * cellXOffset + _firstCube.transform.position.x + newrowXOffset * i, 0, i * cellYOffset + _firstCube.transform.position.z ), Quaternion.Euler(0, rotation,0));
+                cell = _cellBundle.Cells[i * columnCount + j];
+                cellBox = Instantiate(_cellContentBundle.CellContents.Where(content => content.CellType == cell.CellType).Select(pref => pref.ContentPrefab).FirstOrDefault(), new Vector3(j * cellXOffset + _firstCube.transform.position.x + newrowXOffset * i, 0, i * cellYOffset + _firstCube.transform.position.z), Quaternion.Euler(0, rotation, 0));
+                cellBox.name = "Cell " + (i * columnCount + j).ToString();
             }
         }
     }
